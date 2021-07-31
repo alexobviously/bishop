7725a9aa2d184c91c2433c1b6f61d65e3869369d
Alex Baker <iamalexbaker@gmail.com>
2021-07-31 13:24:59 +0700

    More perft positions

--- test/perft.dart
+++ test/perft.dart
@@ -37,9 +37,13 @@ class PerftBenchmark extends BenchmarkBase {
 
 void main() {
   List<PerftBenchmark> perfts = [
-    PerftBenchmark(Positions.STANDARD_DEFAULT, STANDARD, 4, 197281),
+    PerftBenchmark(Positions.STANDARD_DEFAULT, STANDARD, 3, 8902),
     PerftBenchmark(Positions.KIWIPETE, STANDARD, 2, 2039),
     PerftBenchmark(Positions.ROOK_PIN, STANDARD, 4, 43238),
+    PerftBenchmark(Positions.POSITION_4, STANDARD, 3, 9467),
+    //PerftBenchmark(Positions.POSITION_5, STANDARD, 3, 62379), // broken - check promotion to rook
+    //PerftBenchmark(Positions.POSITION_5, STANDARD, 2, 1486),
+    PerftBenchmark(Positions.POSITION_6, STANDARD, 3, 89890),
   ];
   for (PerftBenchmark perft in perfts) {
     perft.report();
