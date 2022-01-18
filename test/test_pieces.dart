e32ec9906e7a9e57f48b92686c27269c59a9d5c9
Alex Baker <iamalexbaker@gmail.com>
2022-01-19 01:25:31 +0700

    Added piece tests

---
+++ test/test_pieces.dart
@@ -0,0 +1,29 @@
+import 'package:bishop/bishop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  List<CountMovesTest> countMoveTests = [
+    CountMovesTest('N', 8),
+    CountMovesTest('Q', 8),
+    CountMovesTest('B', 4),
+    CountMovesTest('fmWfceFifmnD', 4), // pawn
+    CountMovesTest('BN', 12),
+    CountMovesTest('FWDA', 16), // musketeer elephant
+    CountMovesTest('B2ND', 16), // musketeer spider
+    CountMovesTest('FWDsN', 16), // musketeer cannon
+  ];
+  group('Betza - Count Moves', () {
+    for (CountMovesTest t in countMoveTests) {
+      test('Count Moves - ${t.betza}', () {
+        PieceType pt = PieceType.fromBetza(t.betza);
+        expect(pt.moves.length, t.num);
+      });
+    }
+  });
+}
+
+class CountMovesTest {
+  final String betza;
+  final int num;
+  const CountMovesTest(this.betza, this.num);
+}
