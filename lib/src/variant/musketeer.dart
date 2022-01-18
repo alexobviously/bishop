72fa01cbbcb256a285c7681811fb1f6cde7c27a0
Alex Baker <iamalexbaker@gmail.com>
2022-01-18 22:10:25 +0700

    Musketeer definition

---
+++ lib/src/variant/musketeer.dart
@@ -0,0 +1,36 @@
+import 'package:bishop/bishop.dart';
+
+class Musketeer {
+  static const String DEFAULT_FEN = '8/rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/8 w KQkq - 0 1';
+  static PieceType leopard() => PieceType.fromBetza('F2N');
+  static PieceType hawk() => PieceType.fromBetza('ADGH');
+  static PieceType unicorn() => PieceType.fromBetza('NC');
+  static PieceType spider() => PieceType.fromBetza('B2ND');
+  static PieceType fortress() => PieceType.fromBetza('B3vND');
+  static PieceType elephant() => PieceType.fromBetza('FWDA');
+  static PieceType cannon() => PieceType.fromBetza('FWDsN');
+
+  static Variant variant() {
+    Variant standard = Variant.standard();
+    return standard.copyWith(
+      name: 'Musketeer Chess',
+      startPosition: DEFAULT_FEN,
+      gatingMode: GatingMode.FIXED,
+      pieceTypes: standard.pieceTypes
+        ..addAll(
+          {
+            'A': PieceType.archbishop(),
+            'C': PieceType.chancellor(),
+            'D': PieceType.amazon(), // dragon
+            'L': leopard(),
+            'H': hawk(),
+            'U': unicorn(),
+            'S': spider(),
+            'F': fortress(),
+            'E': elephant(),
+            'O': cannon(),
+          },
+        ),
+    );
+  }
+}
