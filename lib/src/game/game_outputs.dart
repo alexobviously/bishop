70d9ff6e6e32a6ae1ca17a1395f0bce341145bab
govind-maheshwari2 <132160327+govind-maheshwari2@users.noreply.github.com>
2024-04-24 10:43:39 +0530

    Fix wrong PGN format when first move is black to play (#78)

--- lib/src/game/game_outputs.dart
+++ lib/src/game/game_outputs.dart
@@ -218,9 +218,14 @@ extension GameOutputs on Game {
     int firstTurn = history.first.turn;
     int turn = firstTurn;
     String pgn = '';
-    for (int i = 0; i < moves.length; i++) {
-      if (i == 0 || turn == Bishop.white) pgn = '$pgn${firstMove + i ~/ 2}. ';
-      if (i == 0 && turn == Bishop.black) pgn = '$pgn..';
+     for (int i = 0; i < moves.length; i++) {
+      if (i == 0 || turn == Bishop.white)
+        pgn =
+            '$pgn${firstMove + i ~/ 2}${(i == 0 && turn == Bishop.black) ? "" : ". "}';
+      if (i == 0 && turn == Bishop.black) {
+        pgn = '$pgn... ';
+        firstMove++;
+      }
       pgn = '$pgn${moves[i]} ';
       turn = turn.opponent;
     }
